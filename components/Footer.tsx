0c38f7b33c034e07e08cc802522335c0517fb03e
hcasado <hcasado@titaniumindustrialsecurity.com>
2026-01-12 15:03:44 +0100

    Add dynamic car make and model selector

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer className="bg-gray-900 border-t border-gray-800">
             <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
-                <div className="flex justify-center space-x-6 md:order-2">
+                {/* <div className="flex justify-center space-x-6 md:order-2">
                     <Link href="https://twitter.com" target="_blank" className="text-gray-400 hover:text-gray-300">
                         <span className="sr-only">Twitter</span>
                         <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
@@ -17,7 +17,7 @@ export const Footer = () => {
                             <path fillRule="evenodd" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19-.14.75-.42 1-.68 1.03-.58.05-1.02-.38-1.58-.75-.88-.58-1.38-.94-2.23-1.5-.99-.65-.35-1.01.22-1.59.15-.15 2.71-2.48 2.76-2.69a.2.2 0 00-.05-.18c-.06-.05-.14-.03-.21-.02-.09.02-1.49.95-4.22 2.79-.4.27-.76.41-1.08.4-.36-.01-1.04-.2-1.55-.37-.63-.2-1.12-.31-1.08-.66.02-.18.27-.36.74-.55 2.92-1.27 4.86-2.11 5.83-2.51 2.78-1.16 3.35-1.36 3.73-1.36.08 0 .27.02.39.12.1.08.13.19.14.27-.01.06.01.24 0 .24z" clipRule="evenodd" />
                         </svg>
                     </Link>
-                </div>
+                </div> */}
                 <div className="mt-8 md:order-1 md:mt-0">
                     <div className="flex justify-center space-x-6 md:justify-start mb-4">
                         <Link href="/" className="text-sm leading-6 text-gray-400 hover:text-white">Inicio</Link>
